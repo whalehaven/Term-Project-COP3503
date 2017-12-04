57bb3ee0a82b22a2c9455e994ab6bb25bdfaebce
whalehaven <33104224+whalehaven@users.noreply.github.com>
2017-12-04 00:21:09 -0500

    Update builder.cpp

--- builder.cpp
+++ builder.cpp
@@ -28,10 +28,100 @@ int main() {
     switch(choice) { //a switch statement is generated that uses the int choice
     case GENERATE_SENTENCE:
       {
-	wordBank->toString();
+
+	//initializes important variables
+	int chooseComp = 2;
+	int sentType = 1;
+	int numNoun = 1;
+	int numAdj = 0;
+	int numVerb = 1;
+	int numAdv = 0;
+	controller *sentence;
+	
+
+	//asks user if they would like to set up the number of nouns/ verbs in the sentence
+	cout << "Would you like to set the number of components? \n1. Yes\n2. No\n";
+	cout << "Input: ";
+	cin >> chooseComp;
+	//std::cout << "chooseComp = " << chooseComp;
+
+	//checks validity of chooseComp
+	while (cin.fail() || chooseComp < 1 || chooseComp > 2) {
+	  cin.clear();
+	  cout << "\nPlease input a valid number.";
+	  cout << "\nWould you like to control the components? \n1. Yes\n2. No\n";
+	  cout << "\nInput: ";
+	  cin >> chooseComp;
+	  cin.ignore(INT_MAX, '\n');
+	}
+
+	switch (chooseComp) {
+	  //if user wants to set components
+	case  1: {
+	  //asks user if they want adjectives or adverbs
+	  cout << "Do you want a basic sentence (1) or a colorful sentence (2)? ";
+	  cin >> sentType;
+	  //checks input validity
+	  while (cin.fail() || sentType < 1 || numNoun > 2) {
+	    cin.clear();
+	    cin.ignore(INT_MAX, '\n');
+	    cout << "\nPlease input a valid number (1-2): ";
+	    cin >> sentType;
+	  }
+		
+	  //asks user for number of nouns
+	  cout << "Please enter the number of nouns (1-3): ";
+	  cin >> numNoun;
+	  //checks input validity
+	  while (cin.fail() || numNoun < 1 || numNoun > 3) {
+	    cin.clear();
+	    cin.ignore(INT_MAX, '\n');
+	    cout << "\nPlease input a valid number (1-3): ";
+	    cin >> numNoun;
+	  }
+	  //if colorful sentence, set numAdj equal to numNouns.
+	  if (sentType == 2) {
+	    numAdj = numNoun;
+	  }
+		
+	  //asks user for number of verbs
+	  cout << "Please enter the number of verbs (1-2): ";
+	  cin >> numVerb;
+	  //checks input validty
+	  while (cin.fail() || numVerb < 1 || numVerb > 2) {
+	    cin.clear();
+	    cin.ignore(INT_MAX, '\n');
+	    cout << "\nPlease input a valid number (1-2): ";
+	    cin >> numVerb;
+	  }
+	  //if colorful sentence, set numAdv equal to numVerb.
+	  if (sentType == 2) {
+	    numAdv = numVerb;
+	  }
+
+
+	  //std::cout << numNoun << " " << numAdj << " " << numVerb << " " << numAdv << std::endl;
+	  sentence = createSentence(numNoun, numAdj, numVerb, numAdv, wordBank); //generate sentence based on num of components
+	  break;
+	}
+	
+	  //if user wants to randomize number of components
+	case 2 : {	
+	  //srand(time(NULL));
+	  numNoun = (rand() % 3) + 1; //generates num 1-3
+	  numAdj = numNoun;
+	  numVerb = (rand() % 2) + 1; //generates num 1-2
+	  numAdv = numVerb;
+	  sentence = createSentence(numNoun, numAdj, numVerb, numAdv, wordBank); //generates sentence based on num of components
+	  break;
+	}
+	   
+
+	}
+	sentence->printSentence(); //prints out generated sentence
 	break;
       }
-  
+      
     case MAD_LIBS:
       cout << "\nPlease choose one of the following: ";
       cout << "\n1. Random mad libs";
@@ -41,7 +131,7 @@ int main() {
 
       if (choice == 1) {
 	//random mad libs
-	}
+      }
 			
       else if (choice == 2) {
 	cout << "\nWelcome to the Mad Libs Game. Please select the corresponding integer to the categories below.";
@@ -59,7 +149,7 @@ int main() {
 	string word5="";
 	string word6="";
 
-	if (madchoice == 1) { //series of questions for holiday mad lib
+	if (madchoice == 1) {
 	  cout << "\nPlease enter a place: ";
 	  cin >> word1;
 	  cout << "Please enter a noun: ";
@@ -72,8 +162,7 @@ int main() {
 	  cin >> word5;
 	  cout << "Please enter another adjective: ";
 	  cin >> word6;
-		
-	  //prints holiday madlib with user inputs
+
 	  cout << "\nYour Holiday Mad Lib";
 	  cout << "\n--------------------";
 	  cout << "\nIn the " << word1 << " we can build a " << word2;
@@ -87,7 +176,7 @@ int main() {
 	  cout << "\nWalking in a " << word6 << " wonderland\n";
         }
 
-	else if (madchoice == 2){ //series of questions for uf mad lib
+	else if (madchoice == 2){
           cout << "\nPlease enter an adjective: ";
 	  cin >> word1;
 	  cout << "Please enter a body part: ";
@@ -100,8 +189,7 @@ int main() {
 	  cin >> word5;
 	  cout << "Please enter a name: ";
 	  cin >> word6;
-		
-	  //prints uf madlib with user inputs
+
           cout << "\nYour UF Mad Lib";
 	  cout << "\n---------------";
 	  cout << "\nDear prof Fuchs,"; 
@@ -113,7 +201,7 @@ int main() {
 	  cout << "\n" << word6 << "\n";
 	}
 	
-        else if (madchoice == 3){ //series of questions for mystery mad lib
+        else if (madchoice == 3){
 	  cout << "\nPlease enter a name: ";
 	  cin >> word1;
 	  cout << "Please enter another name: ";
@@ -127,7 +215,7 @@ int main() {
 	  cout << "Please enter a drink: ";
 	  cin >> word6;
 
-	  //prints mystery madlib with user inputs
+	  
 	  cout << "\nYour Mystery Mad Lib";
 	  cout << "\n--------------------";
 	  cout << "\nHello. My name is Detective " << word1 << ". Supposedly, somebody stole the";
@@ -137,13 +225,13 @@ int main() {
 	  cout << "\nbecause he had an injured costumer.\n";
 	}
 
-	else if (madchoice > 3 || madchoice < 1){ //if the input is invalid, the input is cleared and the user is asked to give input again 
+	else if (madchoice > 3 || madchoice < 1){
           cout << "\nPlease enter a valid number";
 	  cin >> madchoice;
 	}
       }
 	
-      else if (choice > 2 || choice < 1) { //if the input is invalid, the input is cleared and the user is asked to give input again
+      else if (choice > 2 || choice < 1) {
 	cout << "\nPlease enter a valid number";
 	cin >> choice;
       }
@@ -151,20 +239,21 @@ int main() {
 
     case KNOCK_KNOCK:
       {
-      string punchline = wordBank->findTypeOfWord( "noun" )->content; //finds noun from word bank
-      cout << "\nWelcome to the Knock Knock Joke. Please press enter to continue after each statement.";
-      cin.ignore();
-      cout << "\nKnock knock.";
-      cin.ignore();
-      cout << "Who's there?";
-      cin.ignore();
-      cout << punchline; //outputs noun that was found
-      cin.ignore();
-      cout << punchline << " who?"; 
-      cin.ignore();
-      cout << wordBank->findTypeOfWord( "adjective" )->content << " " <<  punchline << "." << std::endl; //finds and outputs adjective from word bank
-      break;
-      //break;
+	srand( time ( NULL ) );
+	string punchline = wordBank->findTypeOfWord( "noun" )->content;
+	cout << "\nWelcome to the Knock Knock Joke. Please press enter to continue after each statement.";
+	cin.ignore();
+	cout << "\nKnock knock.";
+	cin.ignore();
+	cout << "\nWho's there?";
+	cin.ignore();
+	cout << punchline;
+	cin.ignore();
+	cout << punchline << " who";
+	cin.ignore();
+	cout << wordBank->findTypeOfWord( "adjective" )->content << " " <<  punchline << std::endl; //adj + noun
+	break;
+	//break;
       }
     case EXIT:
       break; //if the user selects 4, the while loop is stopped and the program ends
@@ -195,4 +284,381 @@ int menu() {
     cin >> input;
   }
   return input; //the int 'input' is returned
+}
+
+
+controller*
+createSentence(int numNoun, int numAdj, int numVerb, int numAdv, controller *wordBank ) {
+  //initializes counters for the parts of speech
+  srand ( time ( NULL ) );
+  int verbCount = 0;
+  //initializes the punctuations
+  word *period= new word( "punct", "." );
+  word *exclam= new word( "punct", "!" );
+  word *quest= new word( "punct", "?" );
+	
+  bool plural = isPlural(); //calls bool method to determine if the sentence will use singular or plural nouns.
+  bool prepUse = isPlural();
+  //randomly chooses if sentence will contain singular nouns or plural nouns 
+
+  //if plural, use "the" as the article preference
+  word *articleCapThe = new word ("punct", "The");
+  word *articleThe = new word ("punct", "the");
+		
+  //if singular, randomly select "the" or "a/an" as the article preference
+  word *articleCap = new word ("punct", "A");
+  word *article = new word ("punct", "a");
+  word *articleCapAn = new word ("punct", "An");
+  word *articleAn = new word ("punct", "an");
+	
+	
+  controller *randomList = new controller();
+  word *currentword;
+  word *currentNoun;
+  
+  if (numNoun == 1) {
+    currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+    if (isVowel(currentNoun) && !plural) {
+      randomList -> addWord(articleCapAn); //if using singular nouns and noun starts with a vowel, use 'an'
+    }
+    else if (!isVowel(currentNoun) && !plural) {
+      randomList -> addWord(articleCap); //if using singular nouns and noun starts with a consonant, use 'a'
+    }
+    else {
+      randomList -> addWord(articleCapThe); //else use 'the'
+    }
+    if (numAdj != 0) {
+      currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+      randomList -> addWord(currentword);
+    }
+
+    if ( plural )
+      currentNoun->content += "s"; //if using plural nouns, append s
+    
+    randomList -> addWord(currentNoun); //add noun
+
+    
+    for (int i = 0; i < numVerb; i++) { //for loops through all verbs and existing adv 
+      if (numAdv != 0) {
+
+        randomList -> addWord( wordBank->findTypeOfWord( "adverb" )); //adds adv
+      }
+    
+      randomList -> addWord( wordBank->findTypeOfWord( "verb" )); //adds verb
+     
+      if (numVerb == 2 && verbCount != 1) {
+	randomList -> addWord(wordBank->findTypeOfWord( "conjunction" )); //adds conjunction if needed
+      }
+      verbCount++;
+    }	
+    randomList -> addWord(selectPunct(period, exclam, quest)); //adds random punctuation
+
+    return randomList;
+  }
+
+  if (numNoun == 2) {
+    currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+    if (isVowel(currentNoun) && !plural) {
+      randomList -> addWord(articleCapAn); //if using singular nouns and noun starts with a vowel, use 'an'
+    }
+    else if (!isVowel(currentNoun) && !plural) {
+      randomList -> addWord(articleCap); //if using singular nouns and noun starts with a consonant, use 'a'
+    }
+    else {
+      randomList -> addWord(articleCapThe); //else use 'the'
+    }
+    if (numAdj != 0) {
+      currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+      randomList -> addWord(currentword);
+    }
+
+    if ( plural )
+      currentNoun->content += "s"; //if using plural nouns, append s
+     
+    randomList -> addWord(currentNoun); //add noun
+
+    for (int i = 0; i < numVerb; i++) { //for loops through all verbs and existing adv 
+      if (numAdv != 0) {
+
+        randomList -> addWord( wordBank->findTypeOfWord( "adverb" )); //adds adv
+      }
+    
+      randomList -> addWord( wordBank->findTypeOfWord( "verb" )); //adds verb
+     
+      if (numVerb == 2 && verbCount != 1) {
+	randomList -> addWord(wordBank->findTypeOfWord( "conjunction" )); //adds conjunction if needed
+      }
+      verbCount++;
+    }	
+
+    currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+    if (isVowel(currentNoun) && !plural) {
+      randomList -> addWord(articleAn); //if using singular nouns and noun starts with a vowel, use 'an'
+    }
+    else if (!isVowel(currentNoun) && !plural) {
+      randomList -> addWord(article); //if using singular nouns and noun starts with a consonant, use 'a'
+    }
+    else {
+      randomList -> addWord(articleThe); //else use 'the'
+    }
+    if (numAdj != 0) {
+      currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+      randomList -> addWord(currentword);
+    }
+    
+    if ( plural )
+      currentNoun->content += "s"; //if using plural nouns, append s
+    
+    
+    randomList -> addWord(currentNoun); //add noun
+     
+    randomList -> addWord(selectPunct(period, exclam, quest)); //adds random punctuation
+
+    return randomList;
+   
+    
+    //same thing but order is:
+    //articleCap, adjective, noun, adverb, verb, article, adjective, noun, punct.
+  }
+	
+  if (numNoun == 3) {
+    if (prepUse) {
+    
+      for ( int i = 0; i < 2; ++i )
+	{
+
+	  currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+	  if (isVowel(currentNoun) && !plural && i == 0) {
+	    randomList -> addWord(articleCapAn); //if using singular nouns and noun starts with a vowel, use 'an'
+	  }
+	  else if (!isVowel(currentNoun) && !plural && i == 0) {
+	    randomList -> addWord(articleCap); //if using singular nouns and noun starts with a consonant, use 'a'
+	  }
+	  else if (plural && i == 0) {
+	    randomList -> addWord(articleCapThe); //else use 'the'
+	  }
+
+	  
+	  else  if (isVowel(currentNoun) && !plural && i == 1 ) {
+	    randomList -> addWord(articleAn); //if using singular nouns and noun starts with a vowel, use 'an'
+	  }
+	  else if (!isVowel(currentNoun) && !plural && i == 1) {
+	    randomList -> addWord(article); //if using singular nouns and noun starts with a consonant, use 'a'
+	  }
+	  else {
+	    randomList -> addWord(articleThe); //else use 'the'
+	  }
+	  
+	  if (numAdj != 0) {
+	    currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+	    randomList -> addWord(currentword);
+	  }
+
+	  if ( plural )
+	    currentNoun->content += "s"; //if using plural nouns, append s
+    
+	  
+	  randomList -> addWord(currentNoun); //add noun
+
+	  if ( i == 0 )
+	    randomList -> addWord(wordBank->findTypeOfWord( "conjunction" )); //adds conjunction if needed
+	  
+	 
+	}
+
+      for (int i = 0; i < numVerb; i++) { //for loops through all verbs and existing adv 
+	if (numAdv != 0) {
+
+	  randomList -> addWord( wordBank->findTypeOfWord( "adverb" )); //adds adv
+	}
+    
+	randomList -> addWord( wordBank->findTypeOfWord( "verb" )); //adds verb
+     
+	if (numVerb == 2 && verbCount != 1) {
+	  randomList -> addWord(wordBank->findTypeOfWord( "conjunction" )); //adds conjunction if needed
+	}
+	verbCount++;
+      }
+
+      currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+      if (isVowel(currentNoun) && !plural) {
+	randomList -> addWord(articleAn); //if using singular nouns and noun starts with a vowel, use 'an'
+      }
+      else if (!isVowel(currentNoun) && !plural) {
+	randomList -> addWord(article); //if using singular nouns and noun starts with a consonant, use 'a'
+      }
+      else {
+	randomList -> addWord(articleThe); //else use 'the'
+      }
+      if (numAdj != 0) {
+	currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+	randomList -> addWord(currentword);
+      }
+
+      if ( plural )
+	currentNoun->content += "s"; //if using plural nouns, append s
+    
+      randomList -> addWord(currentNoun); //add noun
+
+      randomList -> addWord(selectPunct(period, exclam, quest)); //adds random punctuation
+
+      return randomList;
+
+
+    }
+
+
+
+
+
+
+    else
+      {
+
+	currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+	if (isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(articleCapAn); //if using singular nouns and noun starts with a vowel, use 'an'
+	}
+	else if (!isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(articleCap); //if using singular nouns and noun starts with a consonant, use 'a'
+	}
+	else {
+	  randomList -> addWord(articleCapThe); //else use 'the'
+	}
+	if (numAdj != 0) {
+	  currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+	  randomList -> addWord(currentword);
+	}
+
+	if ( plural )
+	  currentNoun->content += "s"; //if using plural nouns, append s
+    
+	randomList -> addWord(currentNoun); //add noun
+
+    
+	for (int i = 0; i < numVerb; i++) { //for loops through all verbs and existing adv 
+	  if (numAdv != 0) {
+
+	    randomList -> addWord( wordBank->findTypeOfWord( "adverb" )); //adds adv
+	  }
+    
+	  randomList -> addWord( wordBank->findTypeOfWord( "verb" )); //adds verb
+     
+	  if (numVerb == 2 && verbCount != 1) {
+	    randomList -> addWord(wordBank->findTypeOfWord( "conjunction" )); //adds conjunction if needed
+	  }
+	  verbCount++;
+	}
+
+	currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+	if (isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(articleAn); //if using singular nouns and noun starts with a vowel, use 'an'
+	}
+	else if (!isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(article); //if using singular nouns and noun starts with a consonant, use 'a'
+	}
+	else {
+	  randomList -> addWord(articleThe); //else use 'the'
+	}
+	if (numAdj != 0) {
+	  currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+	  randomList -> addWord(currentword);
+	}
+
+	if ( plural )
+	  currentNoun->content += "s"; //if using plural nouns, append s
+    
+	randomList -> addWord(currentNoun); //add noun
+
+	randomList->addWord( wordBank->findTypeOfWord( "preposition" ));
+
+	currentNoun = wordBank->findTypeOfWord( "noun" ); //generates random noun
+    
+	if (isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(articleAn); //if using singular nouns and noun starts with a vowel, use 'an'
+	}
+	else if (!isVowel(currentNoun) && !plural) {
+	  randomList -> addWord(article); //if using singular nouns and noun starts with a consonant, use 'a'
+	}
+	else {
+	  randomList -> addWord(articleThe); //else use 'the'
+	}
+	if (numAdj != 0) {
+	  currentword = wordBank->findTypeOfWord( "adjective" ); //if adj exists, add a random adj
+	  randomList -> addWord(currentword);
+	}
+
+	if ( plural )
+	  currentNoun->content += "s"; //if using plural nouns, append s
+    
+	randomList -> addWord(currentNoun); //add noun
+
+
+	randomList -> addWord(selectPunct(period, exclam, quest)); //adds random punctuation
+
+	return randomList;
+	
+      }
+    
+
+
+    
+    // article, adjective, noun, WITH, article, adjective, noun, punct.
+   
+  }
+  return nullptr;
+}
+
+
+
+//randomly selects 1 or 2 to determine if the sentence will use singular or plural nouns. 
+bool isPlural () {
+  int randomNumber = (rand() % 2) + 1; //generates num 1-2
+  if (randomNumber == 1) {
+    return false; //use singular nouns
+  }
+  else {
+    
+    return true; //use plural nouns
+  }
+}
+
+//randomly selects 1, 2, or 3 to determine the punctuation of the sentence. 
+word* selectPunct (word *period, word *exclam, word *quest) {
+  int randomNumber = (rand() % 3) + 1; //generates num 1-3
+  if (randomNumber == 1) {
+    return period; //use period
+  }
+  else if (randomNumber == 2) {
+    return exclam; //use exclamation point
+  }
+  else {
+    return quest; //use question mark
+  }
+}
+
+//determines if the first letter of the noun starts with a vowel 
+bool isVowel (word *word) {
+  std::string wordcontent = word->content;
+  std::string firstLetter = word->content.substr(0,0);
+  char const *temp = firstLetter.c_str();
+  int value = *temp;
+	
+  switch ( value ) {
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o': 
+  case 'u':
+    return true;
+  default:
+    return false;
+  }
 }
